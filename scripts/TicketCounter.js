28ca1816452fed7651b0b13a8472a901f9f80874
cwelshofer <cwelshofer@gmail.com>
2020-07-28 15:32:23 -0500

    addedworkingcounter

--- scripts/TicketCounter.js
+++ scripts/TicketCounter.js
@@ -1,10 +1,29 @@
-const target = document.querySelector(".customers")
+
+const contentTarget = document.querySelector(".customers")
 const eventHub = document.querySelector("#state-fair")
 
-export const TicketCounter = () => {
-    target.innerHTML = `Total tickets purchased:`
-    eventHub.addEventListener("fullPackageTicketPurchased", => {
+let totalTickets = 0
 
-})
+const ticketPurchased = () => {
+    totalTickets++
+    if(ticketPurchased === "fullPackageTicketPurchased" ) {
+        totalTickets += 4
+    }
+    contentTarget.innerHTML = `Total Tickets Purchased: ${totalTickets}`
+}
+
+
+export const TicketCounter = () => {
+    contentTarget.innerHTML = `Total tickets purchased: ${totalTickets}`
+
+
+    eventHub.addEventListener("rideTicketPurchased", ticketPurchased)
+    eventHub.addEventListener("foodTicketPurchased", ticketPurchased)
+    eventHub.addEventListener("sideshowTicketPurchased", ticketPurchased)
+    eventHub.addEventListener("fullPackageTicketPurchased", ticketPurchased)
+    eventHub.addEventListener("gameTicketPurchased", ticketPurchased)
+
+
+
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 // Import and invoke the ticket booth component function
+import { TicketCounter } from "./TicketCounter.js";
 import { TicketBooth } from "./TicketBooth.js";
 import { RideTicketHolders } from "./rides/RideTicketHolders.js";
 import { FoodTicketHolders } from "./food/FoodTicketHolder.js";
 import { GameTicketHolders } from "./games/GameTicketHolder.js";
 import { SideshowTicketHolders } from "./sideshows/SideshowTicketHolder.js";
+TicketCounter()
 TicketBooth()
 RideTicketHolders()
 FoodTicketHolders()
